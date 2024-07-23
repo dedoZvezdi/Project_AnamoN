289185d5cae5d77dbbda1494d426e17db902ad34
Svetlin Ivanov <svetlin2003ivanov@abv.bg>
2024-07-23 19:52:21 +0300

    More to the db

--- Assets/Script/CardDatabase.cs
+++ Assets/Script/CardDatabase.cs
@@ -26,6 +26,19 @@ public class CardDatabase : MonoBehaviour
         Sprite SpiritofWind_1 = Resources.Load<Sprite>("Images/ga_id_12a");
         Sprite SpiritofSereneWind = Resources.Load<Sprite>("Images/ga_id_13");
         Sprite FragmentedSpiritofWind = Resources.Load<Sprite>("Images/ga_id_14");
+        Sprite InsigniaoftheCorhazi = Resources.Load<Sprite>("Images/ga_id_15");
+        Sprite ArisannaHerbalistProdigy_0 = Resources.Load<Sprite>("Images/ga_id_16");
+        Sprite ArisannaHerbalistProdigy_1 = Resources.Load<Sprite>("Images/ga_id_16a");
+        Sprite LorraineWanderingWarrior_0 = Resources.Load<Sprite>("Images/ga_id_17");
+        Sprite LorraineWanderingWarrior_1 = Resources.Load<Sprite>("Images/ga_id_17a");
+        Sprite TristanUnderhanded_0 = Resources.Load<Sprite>("Images/ga_id_18");
+        Sprite TristanUnderhanded_1 = Resources.Load<Sprite>("Images/ga_id_18a");
+        Sprite MerlinKingslayer_0 = Resources.Load<Sprite>("Images/ga_id_19");
+        Sprite MerlinKingslayer_1 = Resources.Load<Sprite>("Images/ga_id_19a");
+        Sprite MerlinKingslayer_2 = Resources.Load<Sprite>("Images/ga_id_19b");
+        
+
+
 
 
         cardList.Add(new Card(
@@ -325,5 +338,186 @@ public class CardDatabase : MonoBehaviour
             null,//Durability
             null,//Speed
             FragmentedSpiritofWind)); //Image
+
+        cardList.Add(new Card(
+            17, //id
+            new List<string> { "REGALIA", "ITEM" }, //Types
+            new List<string> { "ASSASSIN" }, //Classes
+            new List<string> { "ASSASSIN", "ARTIFACT" }, //SubTypes
+            "LUXEM", //Element
+            "Insignia of the Corhazi", //Name
+            "(3),REST : Put a preparation counter on your champion.\r\n\r\n" +
+            "Class BonusWhenever you activate a prepared card while your influence is six or less, draw a card into your memory.", //Effect
+            false,//isToken
+            0,//CostMemory
+            null,//CostReserve
+            null,//level
+            null,//Life
+            null,//Durability
+            null,//Speed
+            InsigniaoftheCorhazi)); //Image
+
+        cardList.Add(new Card(
+            18, //id
+            new List<string> { "CHAMPION" }, //Types
+            new List<string> { "CLERIC" }, //Classes
+            new List<string> { "CLERIC", "HUMAN" }, //SubTypes
+            "NORM", //Element
+            "Arisanna, Herbalist Prodigy", //Name
+            "On Enter: Gather twice. " +
+            "(To gather, summon a Blightroot, Manaroot, Silvershine, Fraysia, Razorvine, or Springleaf token, chosen at random.)", //Effect
+            false,//isToken
+            1,//CostMemory
+            null,//CostReserve
+            1,//level
+            19,//Life
+            null,//Durability
+            null,//Speed
+            ArisannaHerbalistProdigy_0)); //Image
+
+        cardList.Add(new Card(
+            19, //id
+            new List<string> { "CHAMPION" }, //Types
+            new List<string> { "CLERIC" }, //Classes
+            new List<string> { "CLERIC", "HUMAN" }, //SubTypes
+            "NORM", //Element
+            "Arisanna, Herbalist Prodigy", //Name
+            "On Enter: Gather twice. " +
+            "(To gather, summon a Blightroot, Manaroot, Silvershine, Fraysia, Razorvine, or Springleaf token, chosen at random.)", //Effect
+            false,//isToken
+            1,//CostMemory
+            null,//CostReserve
+            1,//level
+            19,//Life
+            null,//Durability
+            null,//Speed
+            ArisannaHerbalistProdigy_1)); //Image
+
+        cardList.Add(new Card(
+            20, //id
+            new List<string> { "CHAMPION" }, //Types
+            new List<string> { "WARRIOR" }, //Classes
+            new List<string> { "WARRIOR", "HUMAN" }, //SubTypes
+            "NORM", //Element
+            "Lorraine, Wandering Warrior", //Name
+            "On Enter: Materialize a weapon card with memory cost 0 from your material deck.)", //Effect
+            false,//isToken
+            1,//CostMemory
+            null,//CostReserve
+            1,//level
+            20,//Life
+            null,//Durability
+            null,//Speed
+            LorraineWanderingWarrior_0)); //Image
+
+        cardList.Add(new Card(
+            21, //id
+            new List<string> { "CHAMPION" }, //Types
+            new List<string> { "WARRIOR" }, //Classes
+            new List<string> { "WARRIOR", "HUMAN" }, //SubTypes
+            "NORM", //Element
+            "Lorraine, Wandering Warrior", //Name
+            "On Enter: Materialize a weapon card with memory cost 0 from your material deck.", //Effect
+            false,//isToken
+            1,//CostMemory
+            null,//CostReserve
+            1,//level
+            20,//Life
+            null,//Durability
+            null,//Speed
+            LorraineWanderingWarrior_1)); //Image
+
+        cardList.Add(new Card(
+            22, //id
+            new List<string> { "CHAMPION" }, //Types
+            new List<string> { "ASSASSIN" }, //Classes
+            new List<string> { "ASSASSIN", "HUMAN" }, //SubTypes
+            "NORM", //Element
+            "Tristan, Underhanded", //Name
+            "On Enter: You may put a preparation  counter on Tristan. If you don’t, you gain agility 3 for this turn. " +
+            "(Agility 3 — Return three cards from your memory to your hand at the beginning of the end phase.)", //Effect
+            false,//isToken
+            1,//CostMemory
+            null,//CostReserve
+            1,//level
+            19,//Life
+            null,//Durability
+            null,//Speed
+            TristanUnderhanded_0)); //Image
+
+        cardList.Add(new Card(
+            23, //id
+            new List<string> { "CHAMPION" }, //Types
+            new List<string> { "ASSASSIN" }, //Classes
+            new List<string> { "ASSASSIN", "HUMAN" }, //SubTypes
+            "NORM", //Element
+            "Tristan, Underhanded", //Name
+            "On Enter: You may put a preparation  counter on Tristan. If you don’t, you gain agility 3 for this turn. " +
+            "(Agility 3 — Return three cards from your memory to your hand at the beginning of the end phase.)", //Effect
+            false,//isToken
+            1,//CostMemory
+            null,//CostReserve
+            1,//level
+            19,//Life
+            null,//Durability
+            null,//Speed
+            TristanUnderhanded_1)); //Image
+
+        cardList.Add(new Card(
+            24, //id
+            new List<string> { "CHAMPION" }, //Types
+            new List<string> { "MAGE", "WARRIOR" }, //Classes
+            new List<string> { "MAGE", "WARRIOR", "HUMAN" }, //SubTypes
+            "CRUX", //Element
+            "Merlin, Kingslayer", //Name
+            "Merlin Lineage\r\n\r\n" +
+            "At the beginning of your recollection phase, put a level counter on Merlin. " +
+            "Then, if there's an even amount of level counters on Merlin, draw a card and Merlin's attacks get +2 until end of turn.", //Effect
+            false,//isToken
+            3,//CostMemory
+            null,//CostReserve
+            3,//level
+            28,//Life
+            null,//Durability
+            null,//Speed
+            MerlinKingslayer_0)); //Image
+
+        cardList.Add(new Card(
+            25, //id
+            new List<string> { "CHAMPION" }, //Types
+            new List<string> { "MAGE", "WARRIOR" }, //Classes
+            new List<string> { "MAGE", "WARRIOR", "HUMAN" }, //SubTypes
+            "CRUX", //Element
+            "Merlin, Kingslayer", //Name
+            "Merlin Lineage\r\n\r\n" +
+            "At the beginning of your recollection phase, put a level counter on Merlin. " +
+            "Then, if there's an even amount of level counters on Merlin, draw a card and Merlin's attacks get +2 until end of turn.", //Effect
+            false,//isToken
+            3,//CostMemory
+            null,//CostReserve
+            3,//level
+            28,//Life
+            null,//Durability
+            null,//Speed
+            MerlinKingslayer_1)); //Image
+
+        cardList.Add(new Card(
+            26, //id
+            new List<string> { "CHAMPION" }, //Types
+            new List<string> { "MAGE", "WARRIOR" }, //Classes
+            new List<string> { "MAGE", "WARRIOR", "HUMAN" }, //SubTypes
+            "CRUX", //Element
+            "Merlin, Kingslayer", //Name
+            "Merlin Lineage\r\n\r\n" +
+            "At the beginning of your recollection phase, put a level counter on Merlin. " +
+            "Then, if there's an even amount of level counters on Merlin, draw a card and Merlin's attacks get +2 until end of turn.", //Effect
+            false,//isToken
+            3,//CostMemory
+            null,//CostReserve
+            3,//level
+            28,//Life
+            null,//Durability
+            null,//Speed
+            MerlinKingslayer_2)); //Image
     }
 }
