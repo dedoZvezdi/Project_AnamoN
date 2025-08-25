1e003328328ba3b364537b375840da836255a03d
Svetlin Ivanov <115867744+dedoZvezdi@users.noreply.github.com>
2025-08-25 13:57:50 +0300

    Update GA_DECK.gd

--- Scripts/GA_DECK.gd
+++ Scripts/GA_DECK.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var player_deck = ["excalibur-reflected-edge-dtr1e","lu-bu-indomitable-titan-hvn1e-cur","lu-bu-wrath-incarnate-hvn1e-cur","alice-golden-queen-dtr1e-cur","aetheric-calibration-dtrsd","alice-golden-queen-dtr","academy-guide-p24", "absolving-flames-amb","acolyte-of-cultivation-amb","acolyte-of-cultivation-amb"
+var player_deck = ["fabled-ruby-fatestone-hvn1e","excalibur-reflected-edge-dtr1e","lu-bu-indomitable-titan-hvn1e-cur","lu-bu-wrath-incarnate-hvn1e-cur","alice-golden-queen-dtr1e-cur","aetheric-calibration-dtrsd","alice-golden-queen-dtr","academy-guide-p24", "absolving-flames-amb","acolyte-of-cultivation-amb","acolyte-of-cultivation-amb"
 ,"suzaku-vermillion-phoenix-hvn1e-csr","acolyte-of-cultivation-amb","arcane-disposition-doap","arthur-young-heir-evp","suzaku-vermillion-phoenix-hvn1e"]
 
 var card_database_reference
